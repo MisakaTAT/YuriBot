71a59dc89fdb483604b876577f4450a841aea9a2
Zero <i@mikuac.com>
2020-11-27 22:28:09 +0800

    onGroupDecreaseNotice msg add nickname

--- src/main/java/com/mikuac/bot/plugins/GroupJoinAndQuit.java
+++ src/main/java/com/mikuac/bot/plugins/GroupJoinAndQuit.java
@@ -1,5 +1,6 @@
 package com.mikuac.bot.plugins;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lz1998.pbbot.bot.Bot;
 import net.lz1998.pbbot.bot.BotPlugin;
 import net.lz1998.pbbot.utils.Msg;
@@ -7,12 +8,14 @@ import onebot.OnebotEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.util.Objects;
 
 /**
  * 入群欢迎 & 退群提醒
  * @author Zero
  * @date 2020/11/4 14:48
  */
+@Slf4j
 @Component
 public class GroupJoinAndQuit extends BotPlugin {
 
@@ -34,8 +37,15 @@ public class GroupJoinAndQuit extends BotPlugin {
     public int onGroupDecreaseNotice(@NotNull Bot bot, @NotNull OnebotEvent.GroupDecreaseNoticeEvent event) {
         long groupId = event.getGroupId();
         long userId = event.getUserId();
+        String nickName = "昵称获取失败";
+        try {
+            nickName = Objects.requireNonNull(bot.getGroupMemberInfo(groupId, userId, true)).getNickname();
+            log.info("退群检测插件用户名获取成功：[{}] QQ：[{}]",nickName,userId);
+        } catch (NullPointerException e) {
+            log.info("退群检测插件用户名获取失败",e);
+        }
         Msg msg = Msg.builder()
-                .text(userId + "由于精神失常退出了此群");
+                .text(nickName + "(" + userId + ")" + "由于精神失常退出了此群");
         bot.sendGroupMsg(groupId,msg.build(),false);
         return MESSAGE_IGNORE;
     }
