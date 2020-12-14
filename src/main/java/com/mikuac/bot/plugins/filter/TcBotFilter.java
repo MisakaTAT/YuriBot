66118fb962f16509760074692fc0f6f377a43d3b
Zero <i@mikuac.com>
2020-12-14 11:30:17 +0800

    filter tencent group bot

---
+++ src/main/java/com/mikuac/bot/plugins/filter/TcBotFilter.java
@@ -0,0 +1,31 @@
+package com.mikuac.bot.plugins.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import net.lz1998.pbbot.bot.Bot;
+import net.lz1998.pbbot.bot.BotPlugin;
+import onebot.OnebotEvent;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+/**
+ * 过滤腾讯官方机器人
+ * @author Zero
+ * @date 2020/12/14 11:07
+ */
+@Slf4j
+@Component
+public class TcBotFilter extends BotPlugin {
+
+    @Override
+    public int onGroupMessage(@NotNull Bot bot, @NotNull OnebotEvent.GroupMessageEvent event) {
+        long userId = event.getUserId();
+        long startId = 2854196300L;
+        long endId = 2854216399L;
+        if (userId >= startId && userId <= endId) {
+            log.info("已拦截QQ官方机器人消息，ID：[{}]",userId);
+            return MESSAGE_BLOCK;
+        }
+        return MESSAGE_IGNORE;
+    }
+
+}
