6aaa5b804910e6dbcefefc2ace414bd84e2e322f
MisakaTAT <i@mikuac.com>
2021-05-26 10:05:14 +0800

    [Fix]修复群组回复Bot消息触发at

--- src/main/java/com/mikuac/bot/plugins/GroupAtMe.java
+++ src/main/java/com/mikuac/bot/plugins/GroupAtMe.java
@@ -1,16 +1,18 @@
 package com.mikuac.bot.plugins;
 
 import com.mikuac.bot.common.utils.CommonUtils;
-import com.mikuac.bot.common.utils.RegexUtils;
-import com.mikuac.bot.config.RegexConst;
+import com.mikuac.bot.config.Global;
 import lombok.extern.slf4j.Slf4j;
 import net.lz1998.pbbot.bot.Bot;
 import net.lz1998.pbbot.bot.BotPlugin;
 import net.lz1998.pbbot.utils.Msg;
+import onebot.OnebotBase;
 import onebot.OnebotEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author Zero
@@ -19,6 +21,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class GroupAtMe extends BotPlugin {
+
+    private final static String MSG_TYPE = "at";
 
     private CommonUtils commonUtils;
 
@@ -29,15 +33,29 @@ public class GroupAtMe extends BotPlugin {
 
     @Override
     public int onGroupMessage(@NotNull Bot bot, @NotNull OnebotEvent.GroupMessageEvent event) {
-        String msg = event.getRawMessage();
-        String botId = String.valueOf(event.getSelfId());
-        String getAtId = RegexUtils.regex(RegexConst.AT_ME, msg);
-        if (botId.equals(getAtId)) {
-            String imgUrl = commonUtils.getHostAndPort() + "/img/atme.jpg";
-            long userId = event.getUserId();
-            long groupId = event.getGroupId();
-            bot.sendGroupMsg(groupId, Msg.builder().at(userId).image(imgUrl).build(), false);
-            log.info("@BOT 来自群组：[{}]的用户：[{}]", groupId, userId);
+        // 判断是否为回复
+        OnebotBase.Message reply = event.getMessageList().stream().filter(message -> "reply".equals(message.getType())).findFirst().orElse(null);
+        if (reply != null) {
+            return MESSAGE_IGNORE;
+        }
+        // 获取消息链
+        List<OnebotBase.Message> messageChain = event.getMessageList();
+        if (messageChain.size() > 0) {
+            OnebotBase.Message message = messageChain.get(0);
+            // 判断消息类型是否为at，否则return
+            String msgType = message.getType();
+            if (!MSG_TYPE.equals(msgType)) {
+                return MESSAGE_IGNORE;
+            }
+            // 判断被at的是否为机器人
+            long botId = Long.parseLong(message.getDataMap().get("qq"));
+            if (Global.bot_selfId == botId) {
+                String imgUrl = commonUtils.getHostAndPort() + "/img/atme.jpg";
+                long userId = event.getUserId();
+                long groupId = event.getGroupId();
+                bot.sendGroupMsg(groupId, Msg.builder().at(userId).image(imgUrl).build(), false);
+                log.info("@BOT 来自群组：[{}]的用户：[{}]", groupId, userId);
+            }
         }
         return MESSAGE_IGNORE;
     }
