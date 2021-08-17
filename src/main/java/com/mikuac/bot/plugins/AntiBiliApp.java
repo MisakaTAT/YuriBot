9519ab8df896b0eb71e1c37c7e8dfaebd37f3937
MisakaTAT <i@mikuac.com>
2021-08-17 13:48:39 +0800

    [Change]重构AntiBililiApp插件

--- src/main/java/com/mikuac/bot/plugins/AntiBiliApp.java
+++ src/main/java/com/mikuac/bot/plugins/AntiBiliApp.java
@@ -1,5 +1,6 @@
 package com.mikuac.bot.plugins;
 
+import cn.hutool.http.HttpStatus;
 import com.alibaba.fastjson.JSON;
 import com.mikuac.bot.bean.antibili.AntiBiliBean;
 import com.mikuac.bot.bean.antibili.AntiBiliData;
@@ -13,12 +14,13 @@ import com.mikuac.shiro.bot.BotPlugin;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import com.mikuac.shiro.utils.Msg;
+import com.sun.research.ws.wadl.HTTPMethods;
 import lombok.SneakyThrows;
 import org.apache.commons.text.StringEscapeUtils;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -32,19 +34,14 @@ import java.net.URL;
 @Component
 public class AntiBiliApp extends BotPlugin {
 
+    @Resource
     private AntiBiliBean antiBiliBean;
 
-    @Autowired
-    public void setAntiBiliBean(AntiBiliBean antiBiliBean) {
-        this.antiBiliBean = antiBiliBean;
-    }
-
-    public String getRedirectUrlBv(String url) throws IOException {
-        int resCode = 403;
+    private String getRedirectUrlBv(String url) throws IOException {
         HttpURLConnection h = (HttpURLConnection) new URL(url).openConnection();
-        h.setRequestMethod("GET");
+        h.setRequestMethod(HTTPMethods.GET.value());
         h.connect();
-        if (h.getResponseCode() == resCode) {
+        if (h.getResponseCode() == HttpStatus.HTTP_FORBIDDEN) {
             String bvId = RegexUtils.regex(RegexConst.GET_URL_BVID, String.valueOf(h.getURL()));
             h.disconnect();
             return bvId;
@@ -54,18 +51,34 @@ public class AntiBiliApp extends BotPlugin {
         }
     }
 
-    public void getVideoInfo(String bvId) {
+    private void getVideoInfo(String bvId) {
         String result = HttpClientUtils.httpGetWithJson(ApiConst.BILI_VIDEO_INFO_API + bvId, false);
         antiBiliBean = JSON.parseObject(result, AntiBiliBean.class);
+    }
+
+    private void sendMsg(@NotNull Bot bot, long groupId, long userId, AntiBiliData data, AntiBiliStat stat) {
+        String videoUrl = "https://www.bilibili.com/video/";
+        Msg sendMsg = Msg.builder()
+                .img(data.getPic())
+                .text("\n" + data.getTitle())
+                .text("\nUP：" + data.getOwner().getName())
+                .text("\n播放：" + stat.getView() + "  弹幕：" + stat.getDanmaku())
+                .text("\n投币：" + stat.getCoin() + "  点赞：" + stat.getLike())
+                .text("\n评论：" + stat.getReply() + "  分享：" + stat.getShare())
+                .text("\n" + videoUrl + "av" + data.getAid())
+                .text("\n" + videoUrl + data.getBvid());
+        if (groupId != 0L) {
+            bot.sendGroupMsg(groupId, sendMsg.build(), false);
+            return;
+        }
+        bot.sendPrivateMsg(userId, sendMsg.build(), false);
     }
 
     @SneakyThrows
     @Override
     public int onPrivateMessage(@NotNull Bot bot, PrivateMessageEvent event) {
-        String videoUrl = "https://www.bilibili.com/video/";
         String msg = StringEscapeUtils.unescapeHtml4(event.getMessage()).replaceAll("\\\\", "");
         long userId = event.getUserId();
-
         if (msg.matches(RegexConst.ANTI_BILI_MINI_APP)) {
             // 获取qqDocUrl字段的值
             String redirectUrl = RegexUtils.regex(RegexConst.GET_QQ_DOC_URL, msg);
@@ -76,31 +89,19 @@ public class AntiBiliApp extends BotPlugin {
                     getVideoInfo(bvId);
                     AntiBiliData data = antiBiliBean.getData();
                     AntiBiliStat stat = antiBiliBean.getData().getStat();
-                    Msg sendMsg = Msg.builder()
-                            .img(data.getPic())
-                            .text("\n" + data.getTitle())
-                            .text("\nUP：" + data.getOwner().getName())
-                            .text("\n播放：" + stat.getView() + "  弹幕：" + stat.getDanmaku())
-                            .text("\n投币：" + stat.getCoin() + "  点赞：" + stat.getLike())
-                            .text("\n评论：" + stat.getReply() + "  分享：" + stat.getShare())
-                            .text("\n" + videoUrl + "av" + data.getAid())
-                            .text("\n" + videoUrl + data.getBvid());
-                    bot.sendPrivateMsg(userId, sendMsg.build(), false);
+                    sendMsg(bot, 0L, userId, data, stat);
                 }
             }
         }
-
         return MESSAGE_IGNORE;
     }
 
     @SneakyThrows
     @Override
     public int onGroupMessage(@NotNull Bot bot, @NotNull GroupMessageEvent event) {
-        String videoUrl = "https://www.bilibili.com/video/";
         String msg = StringEscapeUtils.unescapeHtml4(event.getMessage()).replaceAll("\\\\", "");
         long userId = event.getUserId();
         long groupId = event.getGroupId();
-
         if (msg.matches(RegexConst.ANTI_BILI_MINI_APP)) {
             // 获取qqDocUrl字段的值
             String redirectUrl = RegexUtils.regex(RegexConst.GET_QQ_DOC_URL, msg);
@@ -111,21 +112,10 @@ public class AntiBiliApp extends BotPlugin {
                     getVideoInfo(bvId);
                     AntiBiliData data = antiBiliBean.getData();
                     AntiBiliStat stat = antiBiliBean.getData().getStat();
-                    Msg sendMsg = Msg.builder()
-                            .at(userId)
-                            .img(data.getPic())
-                            .text("\n" + data.getTitle())
-                            .text("\nUP：" + data.getOwner().getName())
-                            .text("\n播放：" + stat.getView() + "  弹幕：" + stat.getDanmaku())
-                            .text("\n投币：" + stat.getCoin() + "  点赞：" + stat.getLike())
-                            .text("\n评论：" + stat.getReply() + "  分享：" + stat.getShare())
-                            .text("\n" + videoUrl + "av" + data.getAid())
-                            .text("\n" + videoUrl + data.getBvid());
-                    bot.sendGroupMsg(groupId, sendMsg.build(), false);
+                    sendMsg(bot, groupId, userId, data, stat);
                 }
             }
         }
-
         return MESSAGE_IGNORE;
     }
 
